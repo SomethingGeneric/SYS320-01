8830063807e07e150c005f3459157f2e4aecfdf8
Matt C <matthew.compton@mymail.champlain.edu>
2024-02-15 11:12:10 -0500

    week 4 - parsing apache logs

---
+++ week4/Parsing Apache Logs/parselogs.ps1
@@ -0,0 +1,23 @@
+﻿function ApacheLogs1(){
+
+    $logsnotformatted = Get-Content C:\xampp\apache\logs\access.log
+    $tablerecords = @()
+
+    for ($i=0; $i -ne $logsnotformatted.Length; $i++) {
+
+        $words = $logsnotformatted[$i].Split(" ")
+
+        $tablerecords += [PSCustomObject]@{ "IP" = $words[0]
+                                            "Time" = $words[3].Trim('[')
+                                            "Method" = $words[5].Trim('"')
+                                            "Page" = $words[6]
+                                            "Protocol" = $words[7]
+                                            "Response" = $words[8]
+                                            "Referrer" = $words[10]
+                                            "Client" = $words[11..($words.Length)] }
+
+    }
+
+    return $tablerecords | Where-Object { $_.IP -ilike "10.*" }
+
+}

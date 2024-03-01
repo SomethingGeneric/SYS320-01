6ccf3f553e629571f4e0fe29fbbc424ce07373c4
Matt C <matthew.compton@mymail.champlain.edu>
2024-03-01 11:12:57 -0500

    me when i can organize

--- week7/Scheduled Email/main.ps1
+++ week7/Scheduled Email/main.ps1
@@ -1,6 +1,9 @@
+Set-Location C:\Users\champuser\SYS320-01\week7
+
 . (Join-Path $PSScriptRoot .\Configuration.ps1)
 . (Join-Path $PSScriptRoot .\Email.ps1)
 . (Join-Path $PSScriptRoot .\Scheduler.ps1)
+. (Join-Path $PSScriptRoot .\Event-Logs.ps1)
 
 $configuration = readConfiguration
 
